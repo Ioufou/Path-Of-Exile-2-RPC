36366e13821c7d8a0994a97c82c400be2dc72e66
LouFou_ <63572453+Ioufou@users.noreply.github.com>
2025-09-24 22:23:28 +0200

    Delete pre-generated stuff

--- launcher.py
+++
@@ -1,4 +0,0 @@
-import subprocess, sys, os
-
-bat_file = r"C:\Users\frntl\Documents\Path-Of-Exile-2-RPC\launch_poe2_with_rpc.bat"
-subprocess.Popen(["cmd", "/c", bat_file], creationflags=subprocess.CREATE_NEW_CONSOLE)
