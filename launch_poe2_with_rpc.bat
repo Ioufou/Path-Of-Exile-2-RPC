5aa5cd9b1f46353e3022bde3c3d57c2dac066c44
LouFou_ <63572453+Ioufou@users.noreply.github.com>
2025-09-24 22:23:03 +0200

    Delete pre-generated stuff

--- launch_poe2_with_rpc.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start "" "C:\Users\frntl\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\pythonw.exe" "C:\Users\frntl\Documents\Path-Of-Exile-2-RPC\main.py"
-start "" "C:\Program Files (x86)\Steam\steam.exe" -applaunch 2694490
-exit
